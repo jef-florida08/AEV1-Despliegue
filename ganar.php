85adce88c3284b24a3d8f5d6e797bcb9bf2404b7
jef08 <jasfielding@gmail.com>
2025-11-04 19:32:42 +0100

    Crear ganar.php y perder.php. Ahora cuando un jugador gana o pierde acaba en la pagina correspondiente donde puede pulsar un boton para jugar de nuevo.

---
+++ ganar.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Has Ganado</title>
+        <link rel = "stylesheet" href = "style.css">
+    </head>
+    <body>
+        <h1>¡Enhorabuena, has ganado!</h1>
+        <a href = "index.php"><button>Jugar de nuevo</button></a>
+    </body>
+</html>
